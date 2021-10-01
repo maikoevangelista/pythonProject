55a93681ece9fb9e73b2979201fbaf119a7c279b
perkles <maikosilvae@gmail.com>
2021-10-01 13:41:31 -0300

    python projeto

--- ex004 Dissecando uma Variável.py
+++ ex004 Dissecando uma Variável.py
@@ -1,3 +1,13 @@
 # Faça um programa que leia algo pelo teclado
 # E mostre na tela o seu tipo primitivo
 # E todas as informações possíveis sobre ele.
+
+a = input('digite algo: ')
+print(' o tipo primitivo desse valor é', type(a))
+print('So tem espacos: ', a.isspace())
+print('é um numero? ', a.isnumeric())
+print('e alfabetico? ',a.isalpha())
+print('é alfanumerico?', a.isalnum())
+print('esta em maiscula? ',a.isupper())
+print('esta em minusculo? ' ,a.islower())
+print('esta capitalizada? ',a.istitle())
